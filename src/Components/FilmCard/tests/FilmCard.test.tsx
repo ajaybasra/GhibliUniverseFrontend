d79ba5da0352a26326ad6b4bfdf396524c2fc770
Ajay Basra <ajay_b@hotmail.co.nz>
2023-09-01 09:26:53 +1200

    fix: added back and fixed filmcard tests

---
+++ src/Components/FilmCard/tests/FilmCard.test.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import FilmCard from "../FilmCard";
+import axios from "axios";
+import { MemoryRouter } from "react-router-dom";
+
+jest.mock("axios");
+
+describe("FilmCard Component", () => {
+  beforeEach(() => {
+    (axios.get as jest.Mock).mockResolvedValue({
+      data: [
+        {
+          id: "1",
+          title: "Film 1",
+          description: "Description 1",
+          director: "Director 1",
+          composer: "Composer 1",
+          releaseYear: "2022",
+          filmReviewInfo: {
+            averageRating: 9.5,
+            numberOfRatings: 10,
+          },
+        },
+        {
+          id: "2",
+          title: "Film 2",
+          description: "Description 2",
+          director: "Director 2",
+          composer: "Composer 2",
+          releaseYear: "2021",
+          filmReviewInfo: {
+            averageRating: 0,
+            numberOfRatings: 0,
+          },
+        },
+      ],
+    });
+  });
+
+  it("displays film details when hovered", async () => {
+    render(
+      <MemoryRouter>
+        <FilmCard selectedRating={4} setSelectedRating={jest.fn()} />
+      </MemoryRouter>
+    );
+
+    // Wait for the component to render
+    await screen.findByText("Film 1");
+
+    // Get the first card
+    const card = screen.getByText("Film 1");
+
+    // Simulate hovering over the card
+    fireEvent.mouseEnter(card);
+
+    expect(screen.getByText("Director: Director 1")).toBeInTheDocument();
+    expect(screen.getByText("Composer: Composer 1")).toBeInTheDocument();
+    expect(screen.getByText("Synopsis: Description 1")).toBeInTheDocument();
+    expect(screen.getByText("Average Rating: 9.50 (10)")).toBeInTheDocument();
+  });
+
+  it("displays this film has no reviews when a film has no reviews", async () => {
+    render(
+      <MemoryRouter>
+        <FilmCard selectedRating={4} setSelectedRating={jest.fn()} />
+      </MemoryRouter>
+    );
+
+    // Wait for the component to render
+    await screen.findByText("Film 2");
+
+    // Get the first card
+    const card = screen.getByText("Film 2");
+
+    // Simulate hovering over the card
+    fireEvent.mouseEnter(card);
+
+    expect(screen.getByText("This film has no reviews.")).toBeInTheDocument();
+  });
+});
